d5021b3ea989f744724863a6cec7bbc7bbe73211
biraj-phuyal <birajphuyal11@gmail.com>
2026-03-23 16:52:21 +0000

    Initial commit

---
+++ backend/src/index.ts
@@ -0,0 +1,26 @@
+import express from "express"
+import cors from "cors"
+import {ENV} from "./config/env"
+import { clerkMiddleware } from '@clerk/express'
+
+const app = express()
+
+app.use(cors({origin: ENV.FRONTEND_URL}));
+app.use(clerkMiddleware());
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+app.get("/", (req, res) => {
+    const {} = req.body
+
+    res.json({ 
+    message : "Welcome",
+    endpoint : {
+        users: "/api/users",
+        projucts: "/api/products",
+        comments: "/api/comments",
+    },
+    });
+});
+
+app.listen(ENV.PORT, () => console.log("Server is running in PORT:", ENV.PORT))
