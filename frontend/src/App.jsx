2e86ad55b50c52b643bd74925f209bd408c4870a
biraj-phuyal <birajphuyal11@gmail.com>
2026-05-09 16:09:32 +0100

    Starting the frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,26 @@
+import Navbar  from "./components/Navbar"
+import HomePage  from "./pages/HomePage"
+import ProductPage  from "./pages/ProductPage"
+import ProfilePage  from "./pages/ProfilePage"
+import CreatePage  from "./pages/CreatePage"
+import EditProductPage  from "./pages/EditProductPage"
+import { Routes, Route } from "react-router"
 
 
 function App() {
 
   return (
-    <div>
-      <h1 className="text-red-500"> NANANA </h1>
-      <button className="btn btn-primary"> click this</button>
+    <div className="min-h-screen bg-base-100">
+      <Navbar />
+      <main className="max-w-5xl mx-auto px-4 py-8">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/product/:id" element={<ProductPage />} />
+          <Route path="/profile" element={<ProfilePage />} />
+          <Route path="/create" element={<CreatePage />} />
+          <Route path="/edit/:id" element={<EditProductPage />} />
+        </Routes>
+      </main>
     </div>
   )
 }
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,22 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { ClerkProvider } from '@clerk/react'
+import { BrowserRouter } from 'react-router'
+
+
+const PUBLISHAIBLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+
+if (!PUBLISHAIBLE_KEY) {
+  throw new Error("Missing publishable key");
+}
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+     <ClerkProvider publishableKey={PUBLISHAIBLE_KEY}>
+      <BrowserRouter>
+      <App />
+      </BrowserRouter>
+    </ClerkProvider>
   </StrictMode>,
 )
