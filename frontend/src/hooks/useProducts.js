341180ca210b15fbc8c7a3695256b3f17be3a925
biraj-phuyal <birajphuyal11@gmail.com>
2026-05-13 23:07:16 +0100

    Made the good looking UI for the home page and a create product page redirector in it

--- frontend/src/hooks/useProducts.js
+++ frontend/src/hooks/useProducts.js
@@ -2,5 +2,10 @@ import {useQuery} from "@tanstack/react-query"
 import { getAllProducts } from "../lib/api"
 
 export const useProducts = () => {
-    const result = useQuery({queryFn: getAllProducts})
+    const result = useQuery({queryKey: ["products"] ,queryFn: getAllProducts})
+    return result;
 }
+
+export const useCreateProduct = () => {
+  return useMutation({ mutationFn: createProduct });
+};
--- frontend/src/pages/CreatePage.jsx
+++ frontend/src/pages/CreatePage.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import React from "react";
+import { useNavigate } from "react-router";
+import { useCreateProduct } from "../hooks/useProducts";
+import { SparkleIcon } from "lucide-react";
 
 const CreatePage = () => {
-  return (
-    <div>CreatePage</div>
-  )
-}
+  const navigate = useNavigate();
+  const createProduct = useCreateProduct();
+  const [formData, setFormData] = useState({
+    title: "",
+    description: "",
+    imageUrl: "",
+  });
 
-export default CreatePage
+  const handleSubmit = (e) => {};
+  return (
+    <div className="max-w-lg mx-auto">
+      <Link to="/" className="btn btn-ghost btn-sm gap-1 mb-4">
+        <ArrowLeftIcon className="size-4" /> Back
+      </Link>
+
+      <div className="card bg-base-300">
+        <div className="card-body">
+          <h1 className="card-title">
+            <SparkleIcon className="size-5 text-primary" />
+          </h1>
+
+          <form action={}></form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CreatePage;
